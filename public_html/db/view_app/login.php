d7581f7b1c5e0fec789d17fa31aa715d4653a5e8
magnus <gjerde.magnus@gmail.com>
2020-04-11 17:06:24 +0200

    Split if-statements into case

--- public_html/db/view_app/login.php
+++ public_html/db/view_app/login.php
@@ -6,67 +6,70 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 function login($host, $database, $username, $password)
 {
-    try {
-        $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);
+    $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        if ($_POST["request"] == "login") { //else request == "create_account"
-            if (empty($_POST["username"]) || empty($_POST["password"])) {
-                echo "all fields are required";
-            } else {
-                $query = "SELECT * FROM users WHERE mirror_id = :username";
-                $statement = $connect->prepare($query);
-                $statement->execute(['username' => $_POST["username"]]);
-                $data = $statement->fetch(PDO::FETCH_ASSOC);
+    $request = $_POST["request"];
+    $mirror_id = $_POST["mirror_id"];
+    $password = $_POST["password"];
 
-                if (count($data["mirror_id"]) == 0) {
-                    echo "Serial number does not exist";
-                    return null;
-                }
+    if ($request == "login") {
+        if (empty($mirror_id) || empty($password)) {
+            echo "all fields are required";
+        } else {
+            $query = "SELECT * FROM users WHERE mirror_id = :mirror_id";
+            $statement = $connect->prepare($query);
+            $statement->execute(['mirror_id' => $mirror_id]);
+            $data = $statement->fetch(PDO::FETCH_ASSOC);
 
-                if (!$data["first_login"] && $data["password"] == $_POST["password"]) {
-                    continued_login($connect);
-                } else if ($data["first_login"]) {
-                    first_login($connect);
-                } else {
-                    echo "wrong initials";
-                }
+            if (count($data["mirror_id"]) == 0) {
+                echo 2;
+                return 2;
             }
 
-        } else {
-            echo "Wrong request";
+            if ($data["password"] == $password) {
+                continued_login($connect, $mirror_id);
+            } else {
+                echo 3;
+                return 3;
+            }
         }
 
-    } catch (PDOException $error) {
-        $message = $error->getMessage();
-        echo "PDOException " . $message;
-
+    } else if ($request == "first_login") {
+        first_login($connect, $mirror_id, $password);
+    } else {
+        echo 4;
+        return 4;
     }
+
+    return null;
 }
 
-function continued_login($connect)
+function continued_login($connect, $mirror_id)
 {
     $data = [
         'date_cookie' => date("Y-m-d H:i:s"),
-        'username' => $_POST["username"],
+        'mirror_id' => $mirror_id
     ];
 
-    $query = "UPDATE users SET login_cookie=:date_cookie WHERE mirror_id =:username";
+    $query = "UPDATE users SET login_cookie=:date_cookie WHERE mirror_id =:mirror_id";
     $statement = $connect->prepare($query);
     $statement->execute($data);
-    echo "login_success";
+    echo 1;
 }
 
-function first_login($connect){
+function first_login($connect, $mirror_id, $password)
+{
     $data = [
         'date_cookie' => date("Y-m-d H:i:s"),
-        'password' => $_POST["password"],
-        'username' => $_POST["username"],
+        'password' => $password,
+        'mirror_id' => $mirror_id,
         'first_login' => 0,
     ];
 
-    $query = "UPDATE users SET login_cookie=:date_cookie, password=:password, first_login=:first_login WHERE mirror_id =:username";
+    $query = "UPDATE users SET login_cookie=:date_cookie, password=:password, first_login=:first_login WHERE mirror_id =:mirror_id";
     $statement = $connect->prepare($query);
     $statement->execute($data);
-    echo "account_created";
+    echo 1;
+
 }
--- public_html/db/view_app/request_setup_information.php
+++ public_html/db/view_app/request_setup_information.php
@@ -12,16 +12,38 @@ function request_setup_information($host, $database, $username, $password)
     $mirror_id = $_POST["mirror_id"];
     $request = $_POST["request"];
 
-    $stmt = $pdo->prepare("SELECT * FROM setup WHERE mirror_id=?");
+    $stmt = $pdo->prepare("SELECT * FROM view_setup WHERE mirror_id=?");
     $stmt->execute([$mirror_id]);
     $row = $stmt->fetch(PDO::FETCH_NUM);
 
-    if (!empty($row) && $request == "setup_calendar") {
-        updateUserEmail($pdo, $mirror_id);
-    } else if (!empty($row) && $request == "user_data") getUserData($pdo, $mirror_id);
-    else if (!empty($row) && $request == "setup_data") getSetupData($pdo, $mirror_id);
-    else if ($request == "outlook_setup_url") setupOutlookURL($pdo, $mirror_id);
-    else if ($request == "set_weather_location") updateUserWeatherLocation($pdo, $mirror_id);
+
+    if (empty($row)) {
+        echo 2;
+        return null;
+    } else {
+
+
+        switch ($request) {
+            case "user_data":
+                getUserData($pdo, $mirror_id);
+                break;
+            case "setup_data":
+                getSetupData($pdo, $mirror_id);
+                break;
+            case "outlook_setup_url":
+                setupOutlookURL($pdo, $mirror_id);
+                break;
+            case "weather_location":
+                updateUserWeatherLocation($pdo, $mirror_id);
+                break;
+            case "calendar_provider":
+                updateUserEmail($pdo, $mirror_id);
+                break;
+            default:
+                echo 4;
+                return null;
+        }
+    }
 
 }
 
@@ -36,24 +58,17 @@ function getUserData($pdo, $mirror_id)
 
 function getSetupData($pdo, $mirror_id)
 {
-    $stmt = $pdo->prepare("SELECT * FROM setup WHERE mirror_id=:mirror_id");
+    $stmt = $pdo->prepare("SELECT * FROM view_setup WHERE mirror_id=:mirror_id");
     $stmt->execute(['mirror_id' => $mirror_id]);
     $data = $stmt->fetch(PDO::FETCH_ASSOC);
     echo json_encode($data);
 }
 
-function setupNewUser($pdo, $mirror_id)
-{
-    $service_provider = $_POST["service_provider"];
-    $sql = "INSERT INTO setup (mirror_id, service_provider, calendar_setup) VALUES (?, ?, ?)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$mirror_id, $service_provider, 1]);
-}
 
 function updateUserEmail($pdo, $mirror_id)
 {
     $service_provider = $_POST["service_provider"];
-    $sql = "UPDATE setup SET service_provider =:service_provider, calendar_setup=:setup WHERE mirror_id=:mirror_id";
+    $sql = "UPDATE view_setup SET service_provider =:service_provider, calendar_setup=:setup WHERE mirror_id=:mirror_id";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
         'mirror_id' => $mirror_id,
@@ -66,7 +81,7 @@ function updateUserEmail($pdo, $mirror_id)
 function updateUserWeatherLocation($pdo, $mirror_id)
 {
     $weather_location = $_POST["weather_location"];
-    $sql = "UPDATE setup SET weather_city =:weather_location WHERE mirror_id=:mirror_id";
+    $sql = "UPDATE view_setup SET weather_city =:weather_location WHERE mirror_id=:mirror_id";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
         'mirror_id' => $mirror_id,
@@ -77,20 +92,10 @@ function updateUserWeatherLocation($pdo, $mirror_id)
 
 function setupOutlookURL($pdo, $mirror_id)
 {
-    echo "mirror id is: " . $mirror_id;
-    $statement = $pdo->prepare("SELECT calendar_setup FROM setup WHERE mirror_id=:mirror_id");
-    $statement->execute(['mirror_id' => $mirror_id]);
+    $stmt = $pdo->prepare("SELECT outlook_setupURL FROM view_setup WHERE mirror_id=:mirror_id");
+    $stmt->execute(['mirror_id' => $mirror_id]);
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    while ($row = $statement->fetch()) {
-        if ($row[0] == "1") { //Check if calendar setup is active
-            $stmt = $pdo->prepare("SELECT outlook_setupURL FROM setup WHERE mirror_id=:mirror_id");
-            $stmt->execute(['mirror_id' => $mirror_id]);
-            $data = $stmt->fetch(PDO::FETCH_ASSOC);
-            echo urlencode($data["outlook_setupURL"]);
-        } else {
-            echo "db not ready";
-        }
-    }
-
+    echo urlencode($data["outlook_setupURL"]);
 }
 
