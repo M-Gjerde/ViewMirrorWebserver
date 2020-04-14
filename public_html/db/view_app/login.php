7e6f0e15db853c0f4c38bae82705ef658c57b9bf
magnus <gjerde.magnus@gmail.com>
2020-04-14 11:51:13 +0200

    Finished first_setup function

--- public_html/db/view_app/login.php
+++ public_html/db/view_app/login.php
@@ -12,9 +12,10 @@ function login($host, $database, $username, $password)
     $request = $_POST["request"];
     $mirror_id = $_POST["mirror_id"];
     $password = $_POST["password"];
+    $email = $_POST["email"];
 
     if ($request == "login") {
-        if (empty($mirror_id) || empty($password)) {
+        if (empty($email) || empty($password)) {
             echo "all fields are required";
         } else {
             $query = "SELECT * FROM users WHERE mirror_id = :mirror_id";
@@ -58,16 +59,21 @@ function continued_login($connect, $mirror_id)
     echo 1;
 }
 
-function first_login($connect, $mirror_id, $password)
+function first_login($connect, $mirror_id)
 {
+    $email = $_POST["email"]; //TODO return error if not all parameters are found
+    $new_password = $_POST["new_password"];
+    $full_name = $_POST["full_name"];
     $data = [
         'date_cookie' => date("Y-m-d H:i:s"),
-        'password' => $password,
+        'new_password' => $new_password,
+        'name' => $full_name,
+        'email' => $email,
         'mirror_id' => $mirror_id,
         'first_login' => 0,
     ];
 
-    $query = "UPDATE users SET login_cookie=:date_cookie, password=:password, first_login=:first_login WHERE mirror_id =:mirror_id";
+    $query = "UPDATE users SET login_cookie=:date_cookie, password=:new_password, first_login=:first_login, email=:email, name=:name WHERE mirror_id =:mirror_id";
     $statement = $connect->prepare($query);
     $statement->execute($data);
     echo 1;
